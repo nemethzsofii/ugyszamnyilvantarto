04dc2947a20ae54e0803456ce70ee4f179e080e9
nemethzsofii <161205354+nemethzsofii@users.noreply.github.com>
2025-11-15 16:00:14 +0100

    add number col to cases table

--- models.py
+++ models.py
@@ -4,9 +4,19 @@ class Case(db.Model):
     __tablename__ = 'cases'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    number = db.Column(db.String(45), nullable=False)
     name = db.Column(db.String(45), nullable=False)
     client_name = db.Column(db.String(45), nullable=False)
     description = db.Column(db.String(255), nullable=True)
 
     def __repr__(self):
-        return f'<Case {self.id}: {self.name}>'
+        return f'<Case {self.number}: {self.name}>'
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "number": self.number,
+            "name": self.name,
+            "client_name": self.client_name,
+            "description": self.description
+        }
